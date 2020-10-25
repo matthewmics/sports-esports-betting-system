7194518d2b1e32ce509efb8cd12e5ada6055906f
mmiclat@tigernethost.com <mmiclat@tigernethost.com>
2020-10-25 18:15:50 +0800

    Update layout

--- client-side/src/features/matches/MatchesPage.tsx
+++ client-side/src/features/matches/MatchesPage.tsx
@@ -1,22 +1,79 @@
 import { observer } from 'mobx-react-lite'
 import React from 'react'
-import { Grid, GridColumn, Segment } from 'semantic-ui-react';
+import { Button, Divider, Grid, GridColumn, Header, Icon, Image, Label, Segment } from 'semantic-ui-react';
 
 const MatchesPage = () => {
     return (
         <Grid centered>
             <GridColumn width={12}>
                 <Segment.Group>
-                    <Segment>[ICON] ESL ONE <span style={{float: "right", color:'teal'}}>60s from now</span></Segment>
-                    <Segment>Middle</Segment>
-                    <Segment>Bottom</Segment>
+                    <Segment clearing>
+                        <Label basic image>
+                            <img src='/assets/dota2.png' />
+                            DOTA 2
+                        </Label>
+                        <span style={{ float: 'right', color: 'teal', lineHeight: '27px' }}>
+                            12m 06s from now
+                        </span>
+                    </Segment>
+                    <Segment>
+                        <Grid columns={2} stackable textAlign='center'>
+                            <Divider vertical>VS</Divider>
+
+                            <Grid.Row verticalAlign='middle'>
+                                <Grid.Column>
+                                    <Image src='/assets/noimage.png' centered
+                                        size='tiny' />
+                                            Team Secret
+                                </Grid.Column>
+
+                                <Grid.Column>
+                                    <Image src='/assets/noimage.png' centered
+                                        size='tiny' />
+                                            Team Secret
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Segment>
+                    <Segment clearing>
+                        <Button content='View' floated='right' primary />
+                    </Segment>
                 </Segment.Group>
             </GridColumn>
+
             <GridColumn width={12}>
                 <Segment.Group>
-                    <Segment>[ICON] ESL ONE <span style={{float: "right", color:'teal'}}>60s from now</span></Segment>
-                    <Segment>Middle</Segment>
-                    <Segment>Bottom</Segment>
+                    <Segment clearing>
+                        <Label basic image>
+                            <img src='/assets/dota2.png' />
+                            DOTA 2
+                        </Label>
+                        <span style={{ float: 'right', color: 'teal', lineHeight: '27px' }}>
+                            12m 06s from now
+                        </span>
+                    </Segment>
+                    <Segment>
+                        <Grid columns={2} stackable textAlign='center'>
+                            <Divider vertical>VS</Divider>
+
+                            <Grid.Row verticalAlign='middle'>
+                                <Grid.Column>
+                                    <Image src='/assets/noimage.png' centered
+                                        size='tiny' />
+                                            Team Secret
+                                </Grid.Column>
+
+                                <Grid.Column>
+                                    <Image src='/assets/noimage.png' centered
+                                        size='tiny' />
+                                            Team Secret
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                    </Segment>
+                    <Segment clearing>
+                        <Button content='View' floated='right' primary />
+                    </Segment>
                 </Segment.Group>
             </GridColumn>
         </Grid>
