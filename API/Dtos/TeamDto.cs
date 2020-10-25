edb98921d375c3344c09db49311bc7314c301e48
mmiclat@tigernethost.com <mmiclat@tigernethost.com>
2020-10-25 18:15:43 +0800

    Create Dtos

---
+++ API/Dtos/TeamDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Dtos
+{
+    public class TeamDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+}
