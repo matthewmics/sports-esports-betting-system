11222ce73600295a4324a41260a3242d5a4f3141
mmiclat@tigernethost.com <mmiclat@tigernethost.com>
2020-10-25 18:15:35 +0800

    Create MatchesController.cs

---
+++ API/Controllers/MatchesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using API.Dtos;
+
+namespace API.Controllers
+{
+    public class MatchesController : BaseController
+    {
+        private readonly IMapper _mapper;
+
+        public MatchesController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<ActionResult> List()
+        {
+            var matches = await Context.Matches
+                .Include(x => x.TeamA)
+                .Include(x => x.TeamB)
+                .ToListAsync();
+
+            var matchesToReturn = _mapper.Map<ICollection<MatchDto>>(matches);
+
+            return Ok(matchesToReturn);
+        }
+
+    }
+}
