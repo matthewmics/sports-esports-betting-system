aed6272dd8213c2264a7811f991b9b86fac92b52
matthewmics <matthewmics77@gmail.com>
2020-10-29 22:24:34 +0800

    Create LoginForm.tsx

---
+++ client-side/src/features/user/LoginForm.tsx
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react'
+import { Field, Form as FinalForm } from 'react-final-form'
+import { Button, Form, Header } from 'semantic-ui-react'
+import { TextInput } from '../../app/common/forms/TextInput'
+import { RootStoreContext } from '../../app/stores/rootStore'
+
+export const LoginForm = () => {
+    const rootStore = useContext(RootStoreContext);
+    const { closeModal } = rootStore.modalStore;
+    return (
+        <FinalForm onSubmit={(values: any) => console.log(values)}
+            render={({ handleSubmit }) => {
+                return (
+                    <Form onSubmit={handleSubmit} style={{ overflow: 'auto' }}>
+                        <Header as='h1' content='LOGIN' color='teal' />
+                        <Field component={TextInput}
+                            name='email'
+                            placeholder='Email' />
+                        <Field component={TextInput}
+                            name='password'
+                            type='password'
+                            placeholder='Password' />
+                        <Button content='LOGIN' type='submit' primary
+                            floated='right'
+                            icon='lock' />
+                        <Button content='CANCEL'
+                            onClick={closeModal}
+                            floated='right' />
+                    </Form>
+                )
+            }} />
+    )
+}
