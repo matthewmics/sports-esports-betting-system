85f59f6a98d4a8c14f4afe2f59063c128ef69be6
matthewmics <matthewmics77@gmail.com>
2020-10-29 22:24:13 +0800

    Update modalStore.ts

--- client-side/src/app/stores/modalStore.ts
+++ client-side/src/app/stores/modalStore.ts
@@ -3,19 +3,22 @@ import { action, makeObservable, observable } from "mobx";
 export default class ModalStore {
 
     @observable.shallow modal = {
-        open: false
+        open: false,
+        body: null
     };
 
     constructor() {
         makeObservable(this);
     }
 
-    @action openModal = () => {
+    @action openModal = (body: any) => {
         this.modal.open = true;
+        this.modal.body = body;
     }
 
     @action closeModal = () => {
         this.modal.open = false;
+        this.modal.body = null;
     }
 
 }
