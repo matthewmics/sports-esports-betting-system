4cedb03d15735c6d957d308d4c899a6ab16e9b39
matthewmics <matthewmics77@gmail.com>
2020-10-30 22:44:58 +0800

    Populate Prediction Status

---
+++ API/Migrations/20201030143837_PopulatePredictionStatuses.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class PopulatePredictionStatuses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO PredictionStatuses(Id, Name, DisplayText)" +
+                "VALUES " +
+                "(0, 'open', 'Open')," +
+                "(1, 'settled', 'Settled')," +
+                "(2, 'cancelled', 'Cancelled')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
