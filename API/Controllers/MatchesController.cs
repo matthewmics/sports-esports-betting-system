6a7d6fbebbc84f0aedf6aa92a098f914f69cae8e
matthewmics <matthewmics77@gmail.com>
2020-11-04 12:20:06 +0800

    Create functionality for prediction

--- API/Controllers/MatchesController.cs
+++ API/Controllers/MatchesController.cs
@@ -6,16 +6,21 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using AutoMapper;
 using API.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using API.Interfaces;
+using API.Models;
 
 namespace API.Controllers
 {
     public class MatchesController : BaseController
     {
         private readonly IMapper _mapper;
+        private readonly IUserAccessor _userAccessor;
 
-        public MatchesController(IMapper mapper)
+        public MatchesController(IMapper mapper, IUserAccessor userAccessor)
         {
             _mapper = mapper;
+            _userAccessor = userAccessor;
         }
 
         public async Task<ActionResult> List()
@@ -30,6 +35,48 @@ namespace API.Controllers
             var matchesToReturn = _mapper.Map<ICollection<MatchDto>>(matches);
             
             return Ok(matchesToReturn);
+        }
+
+        [Authorize]
+        [HttpPost("/{id}/predictions/{predictionId}/predict")]
+        public async Task<ActionResult> Predict(int id, int predictionId, decimal amount)
+        {
+            var match = await Context.Matches.Include(x => x.Predictions)
+                              .SingleOrDefaultAsync(m => m.Id == id);
+
+            if (match == null)
+                return NotFound(new { error = "Match not found" });
+
+            var prediction = match.Predictions.SingleOrDefault(x => x.Id == predictionId);
+            if(prediction == null)
+                return NotFound(new { error = "Prediction not found" });
+
+            var customer = await Context.Customers.Include(x => x.AppUser)
+                          .Where(a => a.AppUser.UserName == _userAccessor.GetCurrentUsername())
+                          .SingleAsync();
+
+            var userPrediction = await Context.UserPredictions
+                .SingleOrDefaultAsync(x => x.CustomerId == customer.AppUserId &&
+                                           x.PredictionId == prediction.Id);
+
+            if(userPrediction == null)
+            {
+                userPrediction.Customer = customer;
+                userPrediction.Prediction = prediction;
+                userPrediction.Amount = amount;
+                Context.UserPredictions.Add(userPrediction);
+            }
+            else
+            {
+                // logic for updating prediction
+            }
+
+            var success = await Context.SaveChangesAsync() > 0;
+
+            if (success)
+                return Ok(new { message = "Success"}); // show message only for now
+
+            throw new Exception("Problem saving changes");
         }
 
     }
