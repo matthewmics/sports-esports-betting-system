fee5ae09c539a495b9c4b77bf2714d44c269cc8a
matthewmics <matthewmics77@gmail.com>
2020-11-07 23:06:59 +0800

    Update formValidations.ts

--- client-side/src/app/common/forms/formValidations.ts
+++ client-side/src/app/common/forms/formValidations.ts
@@ -1,10 +1,19 @@
 import { createValidator } from "revalidate"
 
 export const isValidEmail = createValidator(
-    message => value => {
-      if (value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
-        return message
-      }
-    },
-    'Invalid email address'
-  )
+  message => value => {
+    if (value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+      return message
+    }
+  },
+  'Invalid email address'
+)
+
+export const isGreaterThan = (n: number) => createValidator(
+  message => value => {    
+    if (value && (!!!Number(value) || Number(value) <= n)) {
+      return message
+    }
+  },
+  field => `${field} must be greater than ${n}`
+)
