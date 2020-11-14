b4414a073f3aa141bce2b93711f2f14e532c4b72
matthewmics <matthewmics77@gmail.com>
2020-11-14 23:33:42 +0800

    Add Sequence Column

---
+++ API/Migrations/20201114140938_AddSequenceToPrediction.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class AddSequenceToPrediction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Sequence",
+                table: "Predictions",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sequence",
+                table: "Predictions");
+        }
+    }
+}
