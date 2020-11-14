71ef7ef92932319f303ada094dc9febc3c66d3c6
matthewmics <matthewmics77@gmail.com>
2020-11-14 23:33:04 +0800

    Create Game.cs

---
+++ API/Models/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Game
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public short Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayText { get; set; }
+
+        public const short Dota2 = 0;
+        public const short Csgo = 1;
+        public const short Sports = 2;
+    }
+}
