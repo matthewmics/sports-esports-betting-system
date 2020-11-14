e6025f939191340a0b6520f311d91f0fc27c9080
matthewmics <matthewmics77@gmail.com>
2020-11-14 23:31:16 +0800

    Add and Update Properties

--- client-side/src/app/models/prediction.ts
+++ client-side/src/app/models/prediction.ts
@@ -2,11 +2,18 @@ import { ITeam } from "./team";
 
 export interface IPrediction {
     id: number;
-    status: string; 
+    sequence: number;
+    predictionStatus: IPredictionStatus; 
     title: string;
     description: string;
     startDate: Date;
     predictionDetails: IPredictionDetails;
+}
+
+export interface IPredictionStatus {
+    id: number;
+    displayText: string;
+    name: string;
 }
 
 export interface IPredictionForm {
