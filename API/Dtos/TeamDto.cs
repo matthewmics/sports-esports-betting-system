7c31a98925c577964188574861b5966cb72f3648
matthewmics <matthewmics77@gmail.com>
2020-12-01 03:55:56 +0800

    Update TeamDto.cs

--- API/Dtos/TeamDto.cs
+++ API/Dtos/TeamDto.cs
@@ -9,6 +9,7 @@ namespace API.Dtos
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
         public string Image { get; set; }
     }
 }
