9c484b554660f89a18f80f9317ff86536b8668a7
matthewmics <matthewmics77@gmail.com>
2020-12-01 03:53:08 +0800

    Add column createdAt

--- client-side/src/app/models/team.ts
+++ client-side/src/app/models/team.ts
@@ -1,5 +1,11 @@
 export interface ITeam {
     id: number;
     name: string;
+    createdAt: Date;
     image: string;
+}
+
+export interface ITeamEnvelope {
+    teams: ITeam[];
+    teamCount: number;
 }
