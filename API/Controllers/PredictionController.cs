a9a8605989e44c330ee5e42996b22b5bd2c14352
matthewmics <matthewmics77@gmail.com>
2020-12-10 10:11:17 +0800

    Prevent controller from processing predict concurrently

--- API/Controllers/PredictionController.cs
+++ API/Controllers/PredictionController.cs
@@ -3,17 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using MediatR;
 using Application.Prediction.Dtos;
+using System.Threading;
+using System;
 
 namespace API.Controllers
 {
     [Route("api/matches")]
     public class PredictionController : BaseController
     {
+        private readonly static SemaphoreSlim _sempaphorePredict
+            = new SemaphoreSlim(1);
 
         [HttpGet("{matchId}/predictions/{predictionId}/details")]
         public async Task<PredictionDetailsDto> PredictionDetails(int matchId, int predictionId)
         {
-            return await Mediator.Send(new Application.Prediction.Details.Query { MatchId = matchId, PredictionId = predictionId});
+            return await Mediator.Send(new Application.Prediction.Details.Query { MatchId = matchId, PredictionId = predictionId });
         }
 
         [Authorize]
@@ -21,14 +25,25 @@ namespace API.Controllers
         public async Task<ActionResult<ActivePredictionDto>> Predict(int matchId, int predictionId,
             [FromBody] Application.Prediction.Predict.Command command)
         {
+            await _sempaphorePredict.WaitAsync();
             command.MatchId = matchId;
             command.PredictionId = predictionId;
-            return await Mediator.Send(command);
+            try
+            {
+                var result = await Mediator.Send(command);
+                _sempaphorePredict.Release();
+                return result;
+            }
+            catch (Exception err)
+            {
+                _sempaphorePredict.Release();
+                throw err;
+            }
         }
 
         [Authorize]
         [HttpPut("{matchId}/predictions/{predictionId}/predict")]
-        public async Task<ActionResult<ActivePredictionDto>> UpdatePrediction(int matchId, int predictionId, 
+        public async Task<ActionResult<ActivePredictionDto>> UpdatePrediction(int matchId, int predictionId,
             [FromBody] Application.Prediction.Predict.Command command)
         {
             command.MatchId = matchId;
