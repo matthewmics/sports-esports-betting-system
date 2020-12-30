d84645550ae8cdfae5051899e612f4c6716317f9
matthewmics <matthewmics77@gmail.com>
2020-12-30 23:26:29 +0800

    Use generics for handling token generator

--- Application/User/TokenResolver.cs
+++ Application/User/TokenResolver.cs
@@ -1,12 +1,13 @@
-﻿using Application.Interfaces;
-using Domain;
+﻿using Domain;
 using AutoMapper;
 using Microsoft.Extensions.Configuration;
 using Application.User.Dtos;
+using Application.Interfaces;
+using System;
 
 namespace Application.User
 {
-    public class TokenResolver : IValueResolver<AppUser, BaseUser, string>
+    public class TokenResolver<T> : IValueResolver<T, BaseUser, string>
     {
         private readonly IJwtTokenGenerator _jwtTokenGenerator;
         private readonly IConfiguration _configuration;
@@ -17,10 +18,23 @@ namespace Application.User
             _configuration = configuration;
         }
 
-        public string Resolve(AppUser source, BaseUser destination, string destMember, ResolutionContext context)
+        public string Resolve(T source, BaseUser destination, string destMember, ResolutionContext context)
         {
-            return _jwtTokenGenerator.GenerateToken(source.Email,
-                    _configuration.GetSection("AppSettings:TokenKey").Value);
+            if(source is AppUser)
+            {
+                AppUser user = source as AppUser;
+                return _jwtTokenGenerator.GenerateToken(user.Email,
+                        _configuration.GetSection("AppSettings:TokenKey").Value);
+            }
+
+            if (source is Wagerer)
+            {
+                Wagerer user = source as Wagerer;
+                return _jwtTokenGenerator.GenerateToken(user.AppUser.Email,
+                        _configuration.GetSection("AppSettings:TokenKey").Value);
+            }
+
+            throw new Exception("Problem generating token");
         }
     }
 }
