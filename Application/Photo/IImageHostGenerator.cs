6433d4592f0063043eb2874fbbe8b3895e32360b
matthewmics <matthewmics77@gmail.com>
2020-12-30 23:30:38 +0800

    Create service for generating host for image

---
+++ Application/Photo/IImageHostGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Photo
+{
+    public interface IImageHostGenerator
+    {
+        string GetHostImage(string imageName);
+    }
+}
---
+++ Infrastructure/Photos/ImageHostGenerator.cs
@@ -0,0 +1,25 @@
+﻿using Application.Photo;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Photos
+{
+    public class ImageHostGenerator : IImageHostGenerator
+    {
+        private readonly IHttpContextAccessor _httpContext;
+
+        public ImageHostGenerator(IHttpContextAccessor httpContext)
+        {
+            _httpContext = httpContext;
+        }
+        public string GetHostImage(string imageName)
+        {
+            return "http://" +
+                _httpContext.HttpContext.Request.Host.Value +
+                "/commons/images/" +
+                imageName;
+        }
+    }
+}
