e530a535ee307e7cd3125977f9ad2b86126ee03b
matthewmics <matthewmics77@gmail.com>
2021-01-05 18:19:55 +0800

    Create API for fetching paypal webhooks

--- API/Controllers/FundsController.cs
+++ API/Controllers/FundsController.cs
@@ -6,18 +6,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json.Serialization;
 
 namespace API.Controllers
 {
     [Authorize]
     public class FundsController : BaseController
     {
-        [HttpPost("paypal/deposit")]
-        public async Task<PaypalOrderDto> PaypalDeposit([FromBody] Deposit.Command command)
+
+        public class WebhookData
         {
-            return await Mediator.Send(command);
+            [JsonPropertyName("event_type")]
+            public string EventType { get; set; }
+
+            public WebhookDataResource Resource { get; set; }
+
+            public class WebhookDataResource
+            {
+                public string Id { get; set; }
+            }
         }
 
+        [AllowAnonymous]
+        [HttpPost("paypal/webhook")]
+        public async Task<MediatR.Unit> PaypalWebhook([FromBody] WebhookData data)
+        {
+            switch (data.EventType)
+            {
+                case "CHECKOUT.ORDER.APPROVED":
+                    var command = new CaptureDeposit.Command { OrderId = data.Resource.Id };
+                    return await Mediator.Send(command);
+                default:
+                    throw new Exception("Problem processing paypal webhook");
+            }
+
+        }
 
         [HttpPost("paypal/captureDeposit")]
         public async Task<MediatR.Unit> PaypalCaptureDeposit([FromBody] CaptureDeposit.Command command)
@@ -25,11 +48,18 @@ namespace API.Controllers
             return await Mediator.Send(command);
         }
 
+        [HttpPost("paypal/deposit")]
+        public async Task<PaypalOrderDto> PaypalDeposit([FromBody] Deposit.Command command)
+        {
+            return await Mediator.Send(command);
+        }
 
         [HttpPost("paypal/withdraw")]
         public async Task<MediatR.Unit> PaypalWithdraw([FromBody] Withdraw.Command command)
         {
             return await Mediator.Send(command);
         }
+
+
     }
 }
