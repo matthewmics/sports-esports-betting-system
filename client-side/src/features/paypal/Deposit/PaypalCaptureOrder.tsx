ff987568df77cc0485f5f5dbfc8dfdb681dccf97
matthewmics <matthewmics77@gmail.com>
2021-01-07 23:24:25 +0800

    Update PaypalCaptureOrder.tsx

--- client-side/src/features/paypal/Deposit/PaypalCaptureOrder.tsx
+++ client-side/src/features/paypal/Deposit/PaypalCaptureOrder.tsx
@@ -1,5 +1,11 @@
 import React, { useEffect } from 'react'
 
+/***********************************************************
+*** I'VE DECIDED TO LET PAYPAL WEBHOOKS HANDLE CAPTURING ***
+*** TO ENSURE USER GETS HIS CREDITS AFTER A SUCCESSFULL  ***
+*** PAYMENT.                                             ***
+/***********************************************************
+*/
 export const PaypalCaptureOrder = () => {
     useEffect(() => {
         window.close();
