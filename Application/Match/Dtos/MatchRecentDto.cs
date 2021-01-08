ebaf417a0e1554fdb122be792e464efee1b56049
matthewmics <matthewmics77@gmail.com>
2021-01-08 18:23:08 +0800

    Create MatchRecentDto.cs

---
+++ Application/Match/Dtos/MatchRecentDto.cs
@@ -0,0 +1,16 @@
+﻿using Application.Team.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Match.Dtos
+{
+    public class MatchRecentDto
+    {
+        public TeamDto TeamA { get; set; }
+        public TeamDto TeamB { get; set; }
+        public TeamDto Winner { get; set; }
+        public int Id { get; set; }
+        public DateTime SettledDate { get; set; }
+    }
+}
