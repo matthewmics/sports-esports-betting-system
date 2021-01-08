b33158117bfa555def07d148322f84dc7bd776a5
matthewmics <matthewmics77@gmail.com>
2021-01-08 20:33:16 +0800

    Change loading to placeholder

--- client-side/src/features/matches/MatchRecent.tsx
+++ client-side/src/features/matches/MatchRecent.tsx
@@ -1,6 +1,6 @@
 import { observer } from 'mobx-react-lite'
 import React, { Fragment, useContext, useEffect } from 'react'
-import { Header, Segment, Label, Image, Loader } from 'semantic-ui-react'
+import { Header, Segment, Label, Image, Placeholder } from 'semantic-ui-react'
 import { history } from '../..'
 import { RootStoreContext } from '../../app/stores/rootStore'
 
@@ -13,26 +13,39 @@ const MatchRecent = () => {
         loadRecentMatches();
     }, [loadRecentMatches]);
 
-    if (loadingMatchRecent && recentMatchesList.length === 0) return <Loader active />
 
     return (
         <Fragment>
-            <div>
-                <Header attached='top' content='Recent matches' />
-            </div>
+            <Header color='teal' attached='top' content='Recent matches' />
             <div className='ui segments' style={{ borderTop: '0', marginTop: '0' }}>
-                {recentMatchesList.map(x =>
-                    <Segment key={x.id} className='flex-even match-recent height-70'
-                        textAlign='center' onClick={() => {
-                        history.push(`/matches/${x.id}`)
-                    }}>
-                        <Label size='mini' corner={x.winner.id === x.teamA.id ? 'left' : 'right'} icon='trophy' color='green' />
-                        <Image src={x.teamA.image || '/assets/noimage.png'}  className='height-41' />
-                        <span className='width-35p'>vs</span>
-                        <Image src={x.teamB.image || '/assets/noimage.png'} className='height-41' />
-                    </Segment>
-                )}
+                {loadingMatchRecent && recentMatchesList.length === 0 ?
+                    <Fragment>
+                        <Segment className='flex-even match-recent height-70'>
+                            <Placeholder style={{ height: '30px', width: '100%' }}>
+                                <Placeholder.Image />
+                            </Placeholder>
+                        </Segment>
+                        <Segment className='flex-even match-recent height-70'>
+                            <Placeholder style={{ height: '30px', width: '100%' }}>
+                                <Placeholder.Image />
+                            </Placeholder>
+                        </Segment>
+                    </Fragment>
+                    :
+                    recentMatchesList.map(x =>
+                        <Segment key={x.id} className='flex-even match-recent height-70'
+                            textAlign='center' onClick={() => {
+                                history.push(`/matches/${x.id}`)
+                            }}>
+                            <Label size='mini' corner={x.winner.id === x.teamA.id ? 'left' : 'right'} icon='trophy' color='green' />
+                            <Image src={x.teamA.image || '/assets/noimage.png'} className='height-41' />
+                            <span className='width-35p'>vs</span>
+                            <Image src={x.teamB.image || '/assets/noimage.png'} className='height-41' />
+                        </Segment>
+                    )
+                }
             </div>
+
         </Fragment>
     )
 }
