ac9cecbce20838fbc819ba698e61a916ea5ad20c
matthewmics <matthewmics77@gmail.com>
2021-01-10 18:02:54 +0800

    Create CommonHub.cs

---
+++ Application/Hubs/CommonHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Hubs
+{
+    public class CommonHub : Hub
+    {
+        public CommonHub()
+        {
+
+        }
+    }
+}
+
